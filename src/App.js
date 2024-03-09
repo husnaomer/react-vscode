4f74496a6069f826e8c2d49fcff98017318e29a8
Husna Omer <husnaomer993@gmail.com>
2024-03-09 20:43:11 +0500

    added axios and loader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>hello world</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Weather city="New york" />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/weather.js
@@ -0,0 +1,22 @@
+import React from "react";
+import axios from "axios";
+import { Circles } from "react-loader-spinner";
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`the weather in ${props.city} is ${response.data.main.temp}`);
+  }
+  let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Circles
+      height="80"
+      width="80"
+      color="#4fa94d"
+      ariaLabel="circles-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
+}
